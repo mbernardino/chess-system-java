16bd512d89f17d1e91ee36af241ef42f4a5dc431
Moisés Bernardino <moisesb_07@hotmail.com>
2019-02-17 20:20:25 -0300

    Primeiro Commit: Class Position

--- src/br/com/estudojava/application/Program.java
+++ src/br/com/estudojava/application/Program.java
@@ -1,9 +1,14 @@
 package br.com.estudojava.application;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Position position = new Position(3,5);
+        System.out.println(position);
+
     }
 
 }
